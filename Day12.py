2dcf9547265df94b737ba1c0f5c6bdd2262ccd4f
Robyn <robyn.rintjema@gmail.com>
2023-12-19 01:56:51 -0500

    modified:   .gitignore 	new file:   Day12.py 	modified:   advent.py

---
+++ Day12.py
@@ -0,0 +1,56 @@
+import re
+from math import comb
+from itertools import combinations 
+
+'''
+#.#.### 1,1,3
+.#...#....###. 1,1,3
+.#.###.#.###### 1,3,1,6
+####.#...#... 4,1,1
+#....######..#####. 1,6,5
+.###.##....# 3,2,1
+'''
+def find(s, ch):
+    return [i for i, ltr in enumerate(s) if ltr == ch], s.count(ch)
+
+def getArrangements(row,groups,pattern):
+    res = 0
+    indexes,n = find(row,'?')
+    #row =list(filter(None, row.split('.')))
+    r = sum(groups) - row.count('#')
+
+    #opts = comb(n,r)
+    combos = list(combinations(indexes,r))
+
+    for c in combos:
+        temp = [char for char in row]
+        for i in c:
+            temp[i] = '#'
+        reg = re.search(pattern,''.join(temp))
+  
+        if(reg):
+            res = res+1
+    return res
+
+
+def splitRow(line):
+    row = line.strip().split(" ")[0]
+    groups =  [int(numeric_string) for numeric_string in (line.strip().split(" ")[1].split(','))]
+
+    pattern = r"[\.?]*"
+
+    for idx,g in enumerate(groups):
+        if idx > 0:
+            pattern = pattern +r"+"
+        p = r"#{"+str(g)+r"}[\.?]"
+        pattern = pattern+p
+    pattern = pattern +r"*"
+    return row,groups,pattern
+
+def day12(f):
+    sum = 0
+    for line in f:
+        row,groups,pattern = splitRow(line)
+        arr = getArrangements(row,groups,pattern)
+        sum = sum+arr
+    print(sum)
--- advent.py
+++ advent.py
@@ -1,5 +1,5 @@
 import sys
-import Day1,Day2,Day3,Day4,Day5,Day6,Day7,Day8,Day9,Day10
+import Day1,Day2,Day3,Day4,Day5,Day6,Day7,Day8,Day9,Day12
 
 func_dict = {
     "Day1.day1":Day1.day1,
@@ -11,7 +11,7 @@ func_dict = {
     "Day7.day7":Day7.day7,
     "Day8.day8":Day8.day8,
     "Day9.day9":Day9.day9,
-    "Day10.day10":Day10.day10
+    "Day12.day12":Day12.day12
 }
 
 
