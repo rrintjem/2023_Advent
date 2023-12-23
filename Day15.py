01254ce69eb410ee287edd82c3355cdcf6cb18ab
Robyn <robyn.rintjema@gmail.com>
2023-12-22 23:09:28 -0500

    modified:   Day15.py

--- Day15.py
+++ Day15.py
@@ -7,13 +7,61 @@ def hash(str):
 
     return val
 
+def splitStep(step):
+    if ('-' in step):
+        step = step.split('-')
+        operation = '-'
+    else:
+        step = step.split('=')
+        operation = '='
+    return step[0],step[1],operation
+
+def removeLens(box,label):
+    #box = {label:lens}
+    if(label in box):
+        del box[label]
+    return box
+
+def addLens(box,label,lens):
+    box[label] = lens
+    return box
+
+def calcPower(box,id):
+    sum = 0
+    id = id+1
+
+    for idx,label in enumerate(box):
+        lens = box[label]
+        slot= idx+1
+        val = id*slot*int(lens)
+        sum = sum+val
+    return sum
+
+
+
 def day15(f):
     f = f.read()
     steps = f.split(',')
     sum = 0
+    boxes = {}
 
     for s in steps:
-        val = hash(s)
+        label,lens, operation = splitStep(s)
+        box = hash(label)
+   
+        if operation == '-' and box in boxes:
+            #remove
+            boxes[box] = removeLens(boxes[box],label)
+            if len(boxes[box]) == 0:
+                del boxes[box]
+        elif operation == '=':
+            #add
+            if box in boxes:
+                boxes[box] = addLens(boxes[box],label,lens)
+            else:
+                boxes[box] = {label:lens}
+    #print(boxes)
+    for x in boxes.keys():
+        val = calcPower(boxes[x],x)
         sum = sum+val
-            
     print(sum)
